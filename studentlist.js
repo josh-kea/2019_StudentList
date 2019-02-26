84117710eae90e6dd5d91cb4055bd4c000488028
Josh <josh0661@kea.stud.dk>
2019-02-26 15:22:57 +0100

    student objects storing all 3 names is working

--- studentlist.js
+++ studentlist.js
@@ -6,7 +6,8 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   getJSON();
-  console.log(JSON)
+
+  console.log(arrayOfStudents)
 
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
 
@@ -27,15 +28,29 @@ function init() {
     house: "--House--"
   }
 
+  const arrayOfStudents = [];
+
   function createStudents(data){
-    data.forEach(student => {
+    data.forEach(stud => {
         let newStudent = Object.create(Student);
 
-        let fullName = student.fullname.split(' ');
-        
-        newStudent.firstname = fullName[0];
-        newStudent.lastname = fullName[1];
-        
+        let fullName = stud.fullname.split(' ');
 
+        //Checking to see if three names exist in a single students fullname
+        if(fullName[2]){
+          newStudent.firstname = fullName[0];
+          newStudent.secondName = fullName[1]; 
+          newStudent.lastname = fullName[2];
+        } else {
+          newStudent.firstname = fullName[0]; 
+          newStudent.lastname = fullName[1];
+        }
+        
+        
+        newStudent.house = stud.house;
+
+        // console.log(newStudent);
+
+        arrayOfStudents.push(newStudent);
       })
   }
