1bc92c4ac5d0ac03dc6998909486f5a538cb7697
Josh <josh0661@kea.stud.dk>
2019-02-26 15:04:15 +0100

    creating Student Objects, successfully splitting the full names

--- studentlist.js
+++ studentlist.js
@@ -2,9 +2,40 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
+
+
 function init() {
-  console.log("init");
+  getJSON();
+  console.log(JSON)
 
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
 
 }
+
+// Function to fetch data from JSON function getJSON(), it will then store them into objects
+
+  function getJSON() {
+    fetch("students1991.json")
+      .then(e => e.json())
+      .then(createStudents);
+  }
+
+  const Student = {
+    firstname: "--First Name--",
+    secondname: "--Second Name--",
+    lastname: "--Last Name--",
+    house: "--House--"
+  }
+
+  function createStudents(data){
+    data.forEach(student => {
+        let newStudent = Object.create(Student);
+
+        let fullName = student.fullname.split(' ');
+        
+        newStudent.firstname = fullName[0];
+        newStudent.lastname = fullName[1];
+        
+
+      })
+  }
